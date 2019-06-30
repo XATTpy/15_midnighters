7578b2f68d86d1cdc8295ebf55c62175f90ceec7
XATTpy <zoomerrr@mail.ru>
2019-06-30 19:55:50 +0300

    update load_attempts

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,24 @@
+import pytz
+import requests
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    query = 'https://devman.org/api/challenges/solution_attempts/'
+    pages = requests.get(query).json()['number_of_pages']
+    for page in range(1, pages+1):
+        payload = {'page': page}
+        records = requests.get(query, params=payload).json()['records']
+        for record in records:
+            username = record['username']
+            timestamp = record['timestamp']
+            timezone = record['timezone']
+            yield username, timestamp, timezone
+
 
 def get_midnighters():
     pass
 
 if __name__ == '__main__':
-  pass
+    gen = load_attempts()
+    for i in gen:
+        print(i)
